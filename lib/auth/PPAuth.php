e8f3289053cc0e5074f11a70416cbbf54b5edab7
Tomasz <tomasz.brunarski@hamer.com.pl>
2014-08-19 15:18:00 +0200

    fir for OAuthRequest

--- lib/auth/PPAuth.php
+++ lib/auth/PPAuth.php
@@ -33,227 +33,7 @@ class AuthSignature {
 	}
 
 }
-//PayPal specific modification ends
-/* Generic exception class
- */
-class OAuthException extends Exception {
-	// pass
-}
 
-class OAuthConsumer {
-	public $key;
-	public $secret;
-
-	function __construct($key, $secret, $callback_url=NULL) {
-		$this->key = $key;
-		$this->secret = $secret;
-		$this->callback_url = $callback_url;
-	}
-
-	function __toString() {
-		return "OAuthConsumer[key=$this->key,secret=$this->secret]";
-	}
-}
-
-class OAuthToken {
-	// access tokens and request tokens
-	public $key;
-	public $secret;
-
-	/**
-	 * key = the token
-	 * secret = the token secret
-	 */
-	function __construct($key, $secret) {
-		$this->key = $key;
-		$this->secret = $secret;
-	}
-
-	/**
-	 * generates the basic string serialization of a token that a server
-	 * would respond to request_token and access_token calls with
-	 */
-	function to_string() {
-		return "oauth_token=" .
-		OAuthUtil::urlencode_rfc3986($this->key) .
-		"&oauth_token_secret=" .
-		OAuthUtil::urlencode_rfc3986($this->secret);
-	}
-
-	function __toString() {
-		return $this->to_string();
-	}
-}
-
-/**
- * A class for implementing a Signature Method
- * See section 9 ("Signing Requests") in the spec
- */
-abstract class OAuthSignatureMethod {
-	/**
-	 * Needs to return the name of the Signature Method (ie HMAC-SHA1)
-	 * @return string
-	 */
-	abstract public function get_name();
-
-	/**
-	 * Build up the signature
-	 * NOTE: The output of this function MUST NOT be urlencoded.
-	 * the encoding is handled in OAuthRequest when the final
-	 * request is serialized
-	 * @param OAuthRequest $request
-	 * @param OAuthConsumer $consumer
-	 * @param OAuthToken $token
-	 * @return string
-	 */
-	abstract public function build_signature($request, $consumer, $token);
-
-	/**
-	 * Verifies that a given signature is correct
-	 * @param OAuthRequest $request
-	 * @param OAuthConsumer $consumer
-	 * @param OAuthToken $token
-	 * @param string $signature
-	 * @return bool
-	 */
-	public function check_signature($request, $consumer, $token, $signature) {
-		$built = $this->build_signature($request, $consumer, $token);
-		return $built == $signature;
-	}
-}
-
-/**
- * The HMAC-SHA1 signature method uses the HMAC-SHA1 signature algorithm as defined in [RFC2104]
- * where the Signature Base String is the text and the key is the concatenated values (each first
- * encoded per Parameter Encoding) of the Consumer Secret and Token Secret, separated by an '&'
- * character (ASCII code 38) even if empty.
- *   - Chapter 9.2 ("HMAC-SHA1")
- */
-class OAuthSignatureMethod_HMAC_SHA1 extends OAuthSignatureMethod {
-	function get_name() {
-		return "HMAC-SHA1";
-	}
-
-	public function build_signature($request, $consumer, $token) {
-		$base_string = $request->get_signature_base_string();
-		$base_string = preg_replace_callback("/(%[A-Za-z0-9]{2})/", array( $this,'replace_callback'), $base_string);//convert base string to lowercase
-		$request->base_string = $base_string;
-
-		$key_parts = array(
-			$consumer->secret,
-			($token) ? $token->secret : ""
-		);
-
-		$key_parts = OAuthUtil::urlencode_rfc3986($key_parts);
-		$key = implode('&', $key_parts);
-		$key = preg_replace_callback("/(%[A-Za-z0-9]{2})/", array( $this,'replace_callback'), $key);//convert to lowercase
-		return base64_encode(hash_hmac('sha1', $base_string, $key, true));
-	}
-	private function replace_callback($match)
-	{
-		return strtolower($match[0]);
-	}
-}
-
-/**
- * The PLAINTEXT method does not provide any security protection and SHOULD only be used
- * over a secure channel such as HTTPS. It does not use the Signature Base String.
- *   - Chapter 9.4 ("PLAINTEXT")
- */
-class OAuthSignatureMethod_PLAINTEXT extends OAuthSignatureMethod {
-	public function get_name() {
-		return "PLAINTEXT";
-	}
-
-	/**
-	 * oauth_signature is set to the concatenated encoded values of the Consumer Secret and
-	 * Token Secret, separated by a '&' character (ASCII code 38), even if either secret is
-	 * empty. The result MUST be encoded again.
-	 *   - Chapter 9.4.1 ("Generating Signatures")
-	 *
-	 * Please note that the second encoding MUST NOT happen in the SignatureMethod, as
-	 * OAuthRequest handles this!
-	 */
-	public function build_signature($request, $consumer, $token) {
-		$key_parts = array(
-			$consumer->secret,
-			($token) ? $token->secret : ""
-		);
-
-		$key_parts = OAuthUtil::urlencode_rfc3986($key_parts);
-		$key = implode('&', $key_parts);
-		$request->base_string = $key;
-
-		return $key;
-	}
-}
-
-/**
- * The RSA-SHA1 signature method uses the RSASSA-PKCS1-v1_5 signature algorithm as defined in
- * [RFC3447] section 8.2 (more simply known as PKCS#1), using SHA-1 as the hash function for
- * EMSA-PKCS1-v1_5. It is assumed that the Consumer has provided its RSA public key in a
- * verified way to the Service Provider, in a manner which is beyond the scope of this
- * specification.
- *   - Chapter 9.3 ("RSA-SHA1")
- */
-abstract class OAuthSignatureMethod_RSA_SHA1 extends OAuthSignatureMethod {
-	public function get_name() {
-		return "RSA-SHA1";
-	}
-
-	// Up to the SP to implement this lookup of keys. Possible ideas are:
-	// (1) do a lookup in a table of trusted certs keyed off of consumer
-	// (2) fetch via http using a url provided by the requester
-	// (3) some sort of specific discovery code based on request
-	//
-	// Either way should return a string representation of the certificate
-	protected abstract function fetch_public_cert(&$request);
-
-	// Up to the SP to implement this lookup of keys. Possible ideas are:
-	// (1) do a lookup in a table of trusted certs keyed off of consumer
-	//
-	// Either way should return a string representation of the certificate
-	protected abstract function fetch_private_cert(&$request);
-
-	public function build_signature($request, $consumer, $token) {
-		$base_string = $request->get_signature_base_string();
-		$request->base_string = $base_string;
-
-		// Fetch the private key cert based on the request
-		$cert = $this->fetch_private_cert($request);
-
-		// Pull the private key ID from the certificate
-		$privatekeyid = openssl_get_privatekey($cert);
-
-		// Sign using the key
-		$ok = openssl_sign($base_string, $signature, $privatekeyid);
-
-		// Release the key resource
-		openssl_free_key($privatekeyid);
-
-		return base64_encode($signature);
-	}
-
-	public function check_signature($request, $consumer, $token, $signature) {
-		$decoded_sig = base64_decode($signature);
-
-		$base_string = $request->get_signature_base_string();
-
-		// Fetch the public key cert based on the request
-		$cert = $this->fetch_public_cert($request);
-
-		// Pull the public key ID from the certificate
-		$publickeyid = openssl_get_publickey($cert);
-
-		// Check the computed signature against the one passed in the query
-		$ok = openssl_verify($base_string, $decoded_sig, $publickeyid);
-
-		// Release the key resource
-		openssl_free_key($publickeyid);
-
-		return $ok == 1;
-	}
-}
 
 class PPOAuthRequest {
 	public $parameters;
@@ -266,7 +46,7 @@ class PPOAuthRequest {
 
 	function __construct($http_method, $http_url, $parameters=NULL) {
 		$parameters = ($parameters) ? $parameters : array();
-		$parameters = array_merge( OAuthUtil::parse_parameters(parse_url($http_url, PHP_URL_QUERY)), $parameters);
+		$parameters = array_merge( PPOAuthUtil::parse_parameters(parse_url($http_url, PHP_URL_QUERY)), $parameters);
 		$this->parameters = $parameters;
 		$this->http_method = $http_method;
 		$this->http_url = $http_url;
@@ -293,10 +73,10 @@ class PPOAuthRequest {
 		// parsed parameter-list
 		if (!$parameters) {
 			// Find request headers
-			$request_headers = OAuthUtil::get_headers();
+			$request_headers = PPOAuthUtil::get_headers();
 
 			// Parse the query-string to find GET parameters
-			$parameters = OAuthUtil::parse_parameters($_SERVER['QUERY_STRING']);
+			$parameters = PPOAuthUtil::parse_parameters($_SERVER['QUERY_STRING']);
 
 			// It's a POST request of the proper content-type, so parse POST
 			// parameters and add those overriding any duplicates from GET
@@ -305,7 +85,7 @@ class PPOAuthRequest {
 				&& strstr($request_headers['Content-Type'],
 					'application/x-www-form-urlencoded')
 			) {
-				$post_data = OAuthUtil::parse_parameters(
+				$post_data = PPOAuthUtil::parse_parameters(
 					file_get_contents(self::$POST_INPUT)
 				);
 				$parameters = array_merge($parameters, $post_data);
@@ -314,7 +94,7 @@ class PPOAuthRequest {
 			// We have a Authorization-header with OAuth data. Parse the header
 			// and add those overriding any duplicates from GET or POST
 			if (isset($request_headers['Authorization']) && substr($request_headers['Authorization'], 0, 6) == 'OAuth ') {
-				$header_parameters = OAuthUtil::split_header(
+				$header_parameters = PPOAuthUtil::split_header(
 					$request_headers['Authorization']
 				);
 				$parameters = array_merge($parameters, $header_parameters);
@@ -330,7 +110,7 @@ class PPOAuthRequest {
 	 */
 	public static function from_consumer_and_token($consumer, $token, $http_method, $http_url, $parameters=NULL) {
 		$parameters = ($parameters) ?  $parameters : array();
-		$defaults = array("oauth_version" => OAuthRequest::$version,
+		$defaults = array("oauth_version" => PPOAuthRequest::$version,
 			// "oauth_nonce" => OAuthRequest::generate_nonce(),
 			"oauth_timestamp" => PPOAuthRequest::generate_timestamp(),
 
@@ -406,7 +186,7 @@ class PPOAuthRequest {
 			$this->get_signable_parameters()
 		);
 
-		$parts = OAuthUtil::urlencode_rfc3986($parts);
+		$parts = PPOAuthUtil::urlencode_rfc3986($parts);
 
 		return implode('&', $parts);
 	}
@@ -453,7 +233,7 @@ class PPOAuthRequest {
 	 * builds the data one would send in a POST request
 	 */
 	public function to_postdata() {
-		return OAuthUtil::build_http_query($this->parameters);
+		return PPOAuthUtil::build_http_query($this->parameters);
 	}
 
 	/**
@@ -462,7 +242,7 @@ class PPOAuthRequest {
 	public function to_header($realm=null) {
 		$first = true;
 		if($realm) {
-			$out = 'Authorization: OAuth realm="' . OAuthUtil::urlencode_rfc3986($realm) . '"';
+			$out = 'Authorization: OAuth realm="' . PPOAuthUtil::urlencode_rfc3986($realm) . '"';
 			$first = false;
 		} else
 			$out = 'Authorization: OAuth';
@@ -474,9 +254,9 @@ class PPOAuthRequest {
 				throw new OAuthException('Arrays not supported in headers');
 			}
 			$out .= ($first) ? ' ' : ',';
-			$out .= OAuthUtil::urlencode_rfc3986($k) .
+			$out .= PPOAuthUtil::urlencode_rfc3986($k) .
 				'="' .
-				OAuthUtil::urlencode_rfc3986($v) .
+				PPOAuthUtil::urlencode_rfc3986($v) .
 				'"';
 			$first = false;
 		}
@@ -554,7 +334,7 @@ class PPOAuthRequest {
 class PPOAuthUtil {
 	public static function urlencode_rfc3986($input) {
 		if (is_array($input)) {
-			return array_map(array('OAuthUtil', 'urlencode_rfc3986'), $input);
+			return array_map(array('PPOAuthUtil', 'urlencode_rfc3986'), $input);
 		} else if (is_scalar($input)) {
 			$tmp1=str_replace('%7E', '~', rawurlencode($input));
 			$tmp2=str_replace(".","%2E",$tmp1);
@@ -580,6 +360,142 @@ class PPOAuthUtil {
 			$op[$k] = $v;
 		}
 		return $op;
+	}
+	//parses string to associative array -modified for PayPal Signature
+
+
+	// This decode function isn't taking into consideration the above
+	// modifications to the encoding process. However, this method doesn't
+	// seem to be used anywhere so leaving it as is.
+	public static function urldecode_rfc3986($string) {
+		return urldecode($string);
+	}
+
+	// Utility function for turning the Authorization: header into
+	// parameters, has to do some unescaping
+	// Can filter out any non-oauth parameters if needed (default behaviour)
+	// May 28th, 2010 - method updated to tjerk.meesters for a speed improvement.
+	//                  see http://code.google.com/p/oauth/issues/detail?id=163
+	public static function split_header($header, $only_allow_oauth_parameters = true) {
+		$params = array();
+		if (preg_match_all('/('.($only_allow_oauth_parameters ? 'oauth_' : '').'[a-z_-]*)=(:?"([^"]*)"|([^,]*))/', $header, $matches)) {
+			foreach ($matches[1] as $i => $h) {
+				$params[$h] = PPOAuthUtil::urldecode_rfc3986(empty($matches[3][$i]) ? $matches[4][$i] : $matches[3][$i]);
+			}
+			if (isset($params['realm'])) {
+				unset($params['realm']);
+			}
+		}
+		return $params;
+	}
+
+	// helper to try to sort out headers for people who aren't running apache
+	public static function get_headers() {
+		if (function_exists('apache_request_headers')) {
+			// we need this to get the actual Authorization: header
+			// because apache tends to tell us it doesn't exist
+			$headers = apache_request_headers();
+
+			// sanitize the output of apache_request_headers because
+			// we always want the keys to be Cased-Like-This and arh()
+			// returns the headers in the same case as they are in the
+			// request
+			$out = array();
+			foreach ($headers AS $key => $value) {
+				$key = str_replace(
+					" ",
+					"-",
+					ucwords(strtolower(str_replace("-", " ", $key)))
+				);
+				$out[$key] = $value;
+			}
+		} else {
+			// otherwise we don't have apache and are just going to have to hope
+			// that $_SERVER actually contains what we need
+			$out = array();
+			if( isset($_SERVER['CONTENT_TYPE']) )
+				$out['Content-Type'] = $_SERVER['CONTENT_TYPE'];
+			if( isset($_ENV['CONTENT_TYPE']) )
+				$out['Content-Type'] = $_ENV['CONTENT_TYPE'];
+
+			foreach ($_SERVER as $key => $value) {
+				if (substr($key, 0, 5) == "HTTP_") {
+					// this is chaos, basically it is just there to capitalize the first
+					// letter of every word that is not an initial HTTP and strip HTTP
+					// code from przemek
+					$key = str_replace(
+						" ",
+						"-",
+						ucwords(strtolower(str_replace("_", " ", substr($key, 5))))
+					);
+					$out[$key] = $value;
+				}
+			}
+		}
+		return $out;
+	}
+
+	// This function takes a input like a=b&a=c&d=e and returns the parsed
+	// parameters like this
+	// array('a' => array('b','c'), 'd' => 'e')
+	public static function parse_parameters( $input ) {
+		if (!isset($input) || !$input) return array();
+
+		$pairs = explode('&', $input);
+
+		$parsed_parameters = array();
+		foreach ($pairs as $pair) {
+			$split = explode('=', $pair, 2);
+			$parameter = PPOAuthUtil::urldecode_rfc3986($split[0]);
+			$value = isset($split[1]) ? PPOAuthUtil::urldecode_rfc3986($split[1]) : '';
+
+			if (isset($parsed_parameters[$parameter])) {
+				// We have already recieved parameter(s) with this name, so add to the list
+				// of parameters with this name
+
+				if (is_scalar($parsed_parameters[$parameter])) {
+					// This is the first duplicate, so transform scalar (string) into an array
+					// so we can add the duplicates
+					$parsed_parameters[$parameter] = array($parsed_parameters[$parameter]);
+				}
+
+				$parsed_parameters[$parameter][] = $value;
+			} else {
+				$parsed_parameters[$parameter] = $value;
+			}
+		}
+		return $parsed_parameters;
+	}
+
+	public static function build_http_query($params) {
+		if (!$params) return '';
+
+		// Urlencode both keys and values
+		$keys = PPOAuthUtil::urlencode_rfc3986(array_keys($params));
+		$values = PPOAuthUtil::urlencode_rfc3986(array_values($params));
+		$params = array_combine($keys, $values);
+
+		// Parameters are sorted by name, using lexicographical byte value ordering.
+		// Ref: Spec: 9.1.1 (1)
+		uksort($params, 'strcmp');
+
+		$pairs = array();
+		foreach ($params as $parameter => $value) {
+			if (is_array($value)) {
+				// If two or more parameters share the same name, they are sorted by their value
+				// Ref: Spec: 9.1.1 (1)
+				// June 12th, 2010 - changed to sort because of issue 164 by hidetaka
+				sort($value, SORT_STRING);
+				foreach ($value as $duplicate_value) {
+					$pairs[] = $parameter . '=' . $duplicate_value;
+				}
+			} else {
+				$pairs[] = $parameter . '=' . $value;
+			}
+		}
+		// For each parameter, the name is separated from the corresponding value by an '=' character (ASCII code 61)
+		// Each name-value pair is separated by an '&' character (ASCII code 38)
+		return implode('&', $pairs);
 	}
 
 }
